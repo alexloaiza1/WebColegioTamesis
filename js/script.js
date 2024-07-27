94bb6ebcb521c81ac25d021df575b157d83fd6e3
Alexander Loaiza <alexloaiza1@gmail.com>
2024-07-26 23:08:33 -0500

    Agrego slide funcional

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
+// swiper-config.js
+
 document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper(".swiper-container", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
     },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
+
+  // Verifica que Swiper esté inicializado correctamente
+  console.log(swiper);
 });
