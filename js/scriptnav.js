d99be2f8056f34ec90eb26efbb7679da73ac5e13
Alexander_Loaiza <Alexander_Loaiza@PRIALEXANDERL>
2024-11-06 12:52:43 -0500

    Agrego imagen a footer

--- js/scriptnav.js
+++ js/scriptnav.js
@@ -2,21 +2,20 @@ const toggleBtn = document.querySelector(".toggle_btn");
 const toggleBtnIcon = document.querySelector(".toggle_btn i");
 const dropDownMenu = document.querySelector(".dropdown_menu");
 
-toggleBtn.onclick = function () {
-  dropDownMenu.classList.toggle("open");
-  const isOpen = dropDownMenu.classList.contains("open");
-  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+// Manejar apertura y cierre del menú
+toggleBtn.onclick = () => {
+  const isOpen = dropDownMenu.classList.toggle("open");
+  toggleBtnIcon.className = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 };
 
-document.querySelectorAll(".dropdown_menu li").forEach((item) => {
-  item.onclick = function (event) {
-    if (
-      !item.classList.contains("nav-item") ||
-      !item.classList.contains("dropdown") ||
-      !item.classList.contains("item")
-    ) {
-      dropDownMenu.classList.remove("open");
-      toggleBtnIcon.classList = "fa-solid fa-bars";
-    }
-  };
-});
+// Delegar el clic en el menú desplegable
+dropDownMenu.onclick = (event) => {
+  const target = event.target;
+  
+  // Cerrar el menú solo si se hace clic en un elemento `li` dentro del menú
+  if (target.tagName === "LI" && !target.classList.contains("nav-item")) {
+    dropDownMenu.classList.remove("open");
+    toggleBtnIcon.className = "fa-solid fa-bars";
+  }
+};
+
